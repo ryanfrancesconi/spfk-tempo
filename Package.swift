8f170cbcebd9c68861ed6979502cc0a68623ded7
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-12 17:55:33 -0700

    Update dependency version minimums to current tags

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.6.0"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.1.0"),
     ],
     targets: [
         .target(
